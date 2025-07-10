7e9e4ef11d925820befa4209a9380148140f81b7
DesyAnggreyni <67110521+DesyAnggreyni@users.noreply.github.com>
2025-07-10 16:15:03 +0700

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-# import tensorflow-cpu==2.10.1 as tf
-from tensorflow.keras import layers, Model
-from sklearn.preprocessing import MinMaxScaler
+# # import tensorflow-cpu==2.10.1 as tf
+# from tensorflow.keras import layers, Model
+# from sklearn.preprocessing import MinMaxScaler
 import joblib
 joblib.dump(scaler, 'scaler.pkl')
 
@@ -24,7 +24,7 @@ if st.button("Prediksi Harga"):
     input_data = np.array([[tahun, fitur_tambahan]])
 
     # Lakukan scaling (jika perlu)
-    scaled_data = scaler.transform(input_data)
+    # scaled_data = scaler.transform(input_data)
 
     # Prediksi harga (generate dari decoder)
     z_points = np.random.normal(size=(1, model.input_shape[1]))  # sampling dari latent space
